0086262533abec79cb4e48ce0d0278e314b34ea2
work <nizamovaarofat@gmail.com>
2025-01-23 17:23:16 +0500

    added home page

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+  //slider
+  $('.slick-slider').each(function () {
+    let slider = $(this);
+    let options = {
+      prevArrow: slider.parent().find('.slider-prev'),
+      nextArrow: slider.parent().find('.slider-next'),
+      infinite: true,
+    }
+    let extraOptions = {}
+    if (slider.hasClass('advantages')) {
+      extraOptions = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+      }
+    }
+    slider.slick($.extend({}, extraOptions, options ));
+  })
   $('.dropdown-hover').on('click', function (e) {
     e.stopPropagation(); 
     $('.dropdown-menu').not($(this).find('.dropdown-menu')).slideUp(200).removeClass('show');
