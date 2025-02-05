a5a82584f5807c42cb5c9c359f6bb183c199653e
work <nizamovaarofat@gmail.com>
2025-02-05 17:27:02 +0500

    fixed header and added page history

--- main.js
+++ main.js
@@ -14,6 +14,40 @@ $(document).ready(function () {
         slidesToScroll: 1,
         arrows: true,
       }
+    } else if (slider.hasClass('history')) {
+      extraOptions = {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+      }
     }
     slider.slick($.extend({}, extraOptions, options ));
   })
